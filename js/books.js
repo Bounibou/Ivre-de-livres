03c60dc007e164d2de99fd060c5bd4e1591cf16a
Bounibou <rakotondrabakooceane@gmail.com>
2026-05-08 12:07:29 +0300

    Update books.js

--- js/books.js
+++ js/books.js
@@ -1,2 +1,20 @@
-[
-]
+fetch("data/books.json")
+  .then(response => response.json())
+  .then(data => {
+
+    const booksContainer = document.getElementById("books-container");
+
+    data.forEach(book => {
+
+      booksContainer.innerHTML += `
+        <div class="book-card">
+          <img src="${book.image}" alt="${book.titre}">
+          <h3>${book.titre}</h3>
+          <p><strong>Auteur :</strong> ${book.auteur}</p>
+          <p>${book.resume}</p>
+        </div>
+      `;
+
+    });
+
+  });
